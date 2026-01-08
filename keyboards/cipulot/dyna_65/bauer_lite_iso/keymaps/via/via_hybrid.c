4a6ea592036bf545b267b3825f8f9ea7b7cf94fa
Cipulot <40441626+Cipulot@users.noreply.github.com>
2026-01-08 02:13:50 +0100

    WIP

--- keyboards/cipulot/dyna_65/bauer_lite_iso/keymaps/via/via_hybrid.c
+++ keyboards/cipulot/dyna_65/bauer_lite_iso/keymaps/via/via_hybrid.c
@@ -110,19 +110,19 @@ void via_config_set_value(uint8_t *data) {
             break;
         }
         case id_apc_actuation_threshold: {
-            uint16_t value = value_data[0] | (value_data[1] << 8);
+            uint16_t value = value_data[1] | (value_data[0] << 8);
             update_keys_field(HYBRID_UPDATE_RUNTIME_ONLY, offsetof(runtime_key_state_t, apc_actuation_threshold), 0, &value, sizeof(uint16_t));
             uprintf("APC Mode Actuation Threshold: %d\n", value);
             break;
         }
         case id_apc_release_threshold: {
-            uint16_t value = value_data[0] | (value_data[1] << 8);
+            uint16_t value = value_data[1] | (value_data[0] << 8);
             update_keys_field(HYBRID_UPDATE_RUNTIME_ONLY, offsetof(runtime_key_state_t, apc_release_threshold), 0, &value, sizeof(uint16_t));
             uprintf("APC Mode Release Threshold: %d\n", value);
             break;
         }
         case id_rt_initial_deadzone_offset: {
-            uint16_t value = value_data[0] | (value_data[1] << 8);
+            uint16_t value = value_data[1] | (value_data[0] << 8);
             update_keys_field(HYBRID_UPDATE_RUNTIME_ONLY, offsetof(runtime_key_state_t, rt_initial_deadzone_offset), 0, &value, sizeof(uint16_t));
             uprintf("Rapid Trigger Mode Initial Deadzone Offset: %d\n", value);
             break;
