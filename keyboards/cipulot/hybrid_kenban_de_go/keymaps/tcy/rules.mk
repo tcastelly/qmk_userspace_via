dcd3b9aba0734f809b2ca6fd1808d880a59510a2
Thomas CASTELLY <thomas@castelly.fr>
2026-02-16 12:00:33 +0100

    right halve fixed, all working

--- keyboards/cipulot/hybrid_kenban_de_go/keymaps/tcy/rules.mk
+++ keyboards/cipulot/hybrid_kenban_de_go/keymaps/tcy/rules.mk
@@ -6,4 +6,8 @@ MOUSEKEY_ENABLE = yes
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
 
+OPT_DEFS += -Wno-error=aggressive-loop-optimizations
+OPT_DEFS += -Wno-error=stringop-overflow
+LDFLAGS += -Wno-error=stringop-overflow
+
 SRC += via_hybrid.c
